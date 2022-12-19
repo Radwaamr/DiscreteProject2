15fa4903b76f3b885bb8785ecc14ed10f9ead0e3
radwaamr9577 <111903049+radwaamr9577@users.noreply.github.com>
2022-12-19 20:40:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting number");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ending number");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("The perfect numbers are :");
+
+            int sum, i, j;
+            for (i = n1; i <= n2; i++)
+            {
+                sum = 0;
+                for (j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum = sum + j;
+
+                }
+                if (sum == i)
+                 
+                    Console.WriteLine(i);
+                
+            }
+
+        }
+    }
+}
